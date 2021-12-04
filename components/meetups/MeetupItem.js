18d36dde3b32811ea6f50b1f6e12066c0a2a7a87
keemtj <keemgreat@gmail.com>
2021-12-04 23:10:26 +0900

    :fire: Remove console.log code

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -3,7 +3,6 @@ import Card from "../ui/Card";
 import classes from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
-  console.log(props);
   const router = useRouter();
 
   function showDetailsHandler() {
